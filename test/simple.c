9cac3d00684b02b6117707113d3d4f3c4fe9ec5c
alberand <albershteyn.andrey@gmail.com>
2020-11-15 09:38:54 +0100

    add: test folder with skeleton for first test

---
+++ test/simple.c
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <FastLED.h>
+
+#include "ledfx.h"
+
+// How many leds in your strip?
+#define NUM_LEDS 10
+
+#define DATA_PIN 7
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void setup() { 
+    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+}
+
+void loop() { 
+  // Turn the LED on, then pause
+  leds[0] = CRGB::Red;
+  FastLED.show();
+  delay(500);
+  // Now turn the LED off, then pause
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(500);
+}
