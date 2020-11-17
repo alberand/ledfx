723bea9b77ed75818ae700a82ea2e20344439c86
alberand <albershteyn.andrey@gmail.com>
2020-11-17 16:20:32 +0100

    add: minimal working example

--- example/src/simple.cpp
+++ example/src/simple.cpp
@@ -6,7 +6,6 @@
 
 // How many leds in your strip?
 #define NUM_LEDS 10
-
 #define DATA_PIN 7
 
 // Define the array of leds
@@ -17,15 +16,7 @@ void setup() {
 }
 
 void loop() { 
-    ledfx_effect(0x48, leds, NUM_LEDS);
+    ledfx_effect(0x44, leds, NUM_LEDS);
     FastLED.show();
-    delay(ledfx_get_config(0x48)->delay);
-  // Turn the LED on, then pause
-  // leds[0] = CRGB::Red;
-  // FastLED.show();
-  // delay(500);
-  // Now turn the LED off, then pause
-  // leds[0] = CRGB::Black;
-  // FastLED.show();
-  // delay(500);
+    delay(ledfx_get_config(0x44)->delay);
 }
