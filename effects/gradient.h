8436c86a7af762aa4a6835059bc420d72873a4f3
alberand <albershteyn.andrey@gmail.com>
2020-12-06 07:10:21 +0100

    fix: gradient effect

--- effects/gradient.h
+++ effects/gradient.h
@@ -6,13 +6,14 @@
 void gradient(CRGB* leds, uint16_t num_leds, const struct animation_config* config)
 {
     uint16_t start = 0;
-    uint16_t len = int(num_leds/(animation_t.num - 1));
+    uint16_t len = (num_leds/(animation_t.num - 1));
 
-    for(uint8_t i = 1; i < animation_t.num; i++) {
+    for(uint8_t i = 0; i < animation_t.num - 1; i++) {
         fill_gradient_RGB(leds, start, animation_t.params[i], \
                           start + len, animation_t.params[i + 1]);
         start = start + len;
     }
+
 }
 
 static struct animation_config gradient_config = 
