3b5f401e5760b7d4b7571ab8037693ae8fc6a0f1
alberand <albershteyn.andrey@gmail.com>
2020-12-26 15:53:33 +0100

    add: nice blinking animation

--- effects/blink.h
+++ effects/blink.h
@@ -2,31 +2,44 @@
 #define __BLINK_H__
 
 #include <ledfx.h>
+#include <math.h>
 
 void blink(CRGB* leds, uint16_t num_leds, const struct animation_config* config)
 {
     const CRGB color = CRGB(animation_t.params[0]);
-    
-    uint16_t multiplier = 1;
-    if(animation_t.params[1] != 0){
-        multiplier = animation_t.params[1];
-    }
-    uint16_t period = num_leds*multiplier;
+    const float smoothness = animation_t.params[1];
+    const uint16_t pause = animation_t.params[2];
 
-    if (animation_t.iteration == period){
+    // reset the loop
+    if(animation_t.iteration == (uint16_t)smoothness + pause){
         animation_t.iteration = 0;
     }
 
-    if(animation_t.iteration == 0){
-        for( uint16_t i = 0; i < num_leds; ++i) {
-            leds[i] = 0;
-        }
+    // pause between blinks
+    if(animation_t.iteration > (uint16_t)smoothness){
+        return;
     }
 
-    if(animation_t.iteration == (int)period/2){
-        for( uint16_t i = 0; i < num_leds; ++i) {
-            leds[i] = color;
-        }
+#define GAUSS
+
+#ifdef CIRCLE
+    const float c = 255.0*sqrt(1.0 -  pow(abs((2.0*(animation_t.iteration/smoothness))-1.0),2.0));
+#endif
+
+#ifdef TRIAN
+    const float c = 255.0*(1.0 -  abs((2.0*(animation_t.iteration/smoothness))-1.0));;
+#endif
+
+#ifdef GAUSS
+    float gamma = 0.14;
+    float beta = 0.5; 
+    const float c = 255.0*(exp(-(pow(((animation_t.iteration/smoothness)-beta)/gamma,2.0))/2.0));
+#endif
+
+    FastLED.setBrightness((int)c);
+
+    for( uint16_t i = 0; i < num_leds; ++i) {
+        leds[i] = color;
     }
 }
 
