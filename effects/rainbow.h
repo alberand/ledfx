7f1f83143fb0af10ca81cb8f0e13bbf58a598eb7
alberand <albershteyn.andrey@gmail.com>
2021-01-19 14:20:00 +0100

    add: rainbow implementation

--- effects/rainbow.h
+++ effects/rainbow.h
@@ -3,53 +3,28 @@
 
 #include <ledfx.h>
 
-void rainbow(CRGB* leds, uint16_t num_leds, const struct animation_config* config)
-{
-    static uint16_t sPseudotime = 0;
-    static uint16_t sLastMillis = 0;
-    static uint16_t sHue16 = 0;
-
-    uint8_t sat8 = beatsin88( 87, 220, 250);
-    uint8_t brightdepth = beatsin88( 341, 96, 224);
-    uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
-    uint8_t msmultiplier = beatsin88(147, 23, 60);
-
-    uint16_t hue16 = sHue16;//gHue * 256;
-    uint16_t hueinc16 = beatsin88(113, 1, 3000);
-
-    uint16_t ms = millis();
-    uint16_t deltams = ms - sLastMillis ;
-    sLastMillis  = ms;
-    sPseudotime += deltams * msmultiplier;
-    sHue16 += deltams * beatsin88( 400, 5,9);
-    uint16_t brightnesstheta16 = sPseudotime;
-
-    for( uint16_t i = 0 ; i < num_leds; i++) {
-        hue16 += hueinc16;
-        uint8_t hue8 = hue16 / 256;
-
-        brightnesstheta16  += brightnessthetainc16;
-        uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
-
-        uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
-        uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
-        bri8 += (255 - brightdepth);
-
-        CRGB newcolor = CHSV( hue8, sat8, bri8);
-
-        uint16_t pixelnumber = i;
-        pixelnumber = (num_leds-1) - pixelnumber;
-
-        nblend( leds[pixelnumber], newcolor, 64);
-    }
+uint32_t color_wheel(uint8_t pos) {
+  	pos = 255 - pos;
+  	if(pos < 85) {
+    	return ((uint32_t)(255 - pos * 3) << 16) | ((uint32_t)(0) << 8) | (pos * 3);
+  	} else if(pos < 170) {
+    	pos -= 85;
+    	return ((uint32_t)(0) << 16) | ((uint32_t)(pos * 3) << 8) | (255 - pos * 3);
+  	} else {
+    	pos -= 170;
+    	return ((uint32_t)(pos * 3) << 16) | ((uint32_t)(255 - pos * 3) << 8) | (0);
+  	}
 }
 
-static struct animation_config rainbow_config = 
-{
-    .id = 0x44,
-    .leds_update = rainbow,
-    .num = 0,
-};
+void rainbow() {
+	uint8_t offset = animation_t.iteration % 255;
 
+	uint8_t step = ledfx_get_param(1);
+
+	for(int i = 0; i < animation_t.num_leds; i++){
+		const uint8_t pos = offset + i + step;
+		animation_t.leds[i] = ledfx_int_to_color(color_wheel(pos));
+	}
+}
 
 #endif // __RAINBOW_H__
