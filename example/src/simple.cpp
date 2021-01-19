c1a4f660937918c6e603fb8dde0fe7444f39b1f4
alberand <albershteyn.andrey@gmail.com>
2021-01-19 14:38:46 +0100

    fix: example uses FastLED to show

--- example/src/simple.cpp
+++ example/src/simple.cpp
@@ -4,23 +4,33 @@
 #include "ledfx.h"
 
 // How many leds in your strip?
-#define NUM_LEDS 10
+#define NUM_LEDS 200
 #define DATA_PIN 7
 
 // Define the array of leds
-CRGB leds[NUM_LEDS];
+CRGB fastleds[NUM_LEDS];
+
+ledfx_color leds[NUM_LEDS];
 
 void setup() { 
-    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(fastleds, NUM_LEDS);
 
-    ledfx_init();
-    ledfx_set_param(0, 0xFF0000);
-    ledfx_set_param(1, 0);
-    ledfx_set_param(2, NUM_LEDS);
+    ledfx_init((ledfx_color*)&leds, NUM_LEDS);
+    ledfx_add_param(0xff00ff);
+    ledfx_add_param(10);
+    ledfx_add_param(0);
+    ledfx_effect(1);
+}
+
+void copy(ledfx_color* leds, CRGB* fastleds, uint16_t len){
+    for(int i = 0; i < len; i++){
+        fastleds[i] = ledfx_color_to_int(leds[i]);
+    }
 }
 
 void loop() { 
-    ledfx_effect(0x41, leds, NUM_LEDS);
+    ledfx_service();
+    copy(leds, fastleds, NUM_LEDS);
     FastLED.show();
-    delay(10);
+    delay(100);
 }
