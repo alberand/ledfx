9ddc0e400d3ba92f3c14cb2202c186404afdf580
alberand <albershteyn.andrey@gmail.com>
2021-08-17 16:31:36 +0200

    add: color wipe effect

--- effects/color_wipe.h
+++ effects/color_wipe.h
@@ -3,37 +3,29 @@
 
 #include <ledfx.h>
 
-#define POSITION_IN_SECOND_HALF(i, p) (i >= (uint16_t)int(p/2))
-#define POSITION_IN_FIRST_HALF(i, p) (i < (uint16_t)int(p/2))
+#define POSITION_IN_SECOND_HALF(i, p) (i >= (uint16_t)(p/2))
+#define POSITION_IN_FIRST_HALF(i, p) (i < (uint16_t)(p/2))
 
-void color_wipe(CRGB* leds, uint16_t num_leds, const struct animation_config* config)
+void color_wipe()
 {
-    const CRGB color = animation_t.params[0];
+    const ledfx_color color = ledfx_color_init(ledfx_get_param(0));
 
     uint32_t led_idx = 0;
-    uint32_t period = num_leds*4;
+    const uint32_t period = animation_t.num_leds*4;
 
     if(animation_t.iteration%4 == 0) {
-        led_idx = (animation_t.iteration/4)%num_leds;
+        led_idx = (animation_t.iteration/4)%animation_t.num_leds;
     } else {
         return;
     }
 
     if(POSITION_IN_FIRST_HALF((animation_t.iteration/4)%period, period)) {
-        leds[led_idx] = color;
+        ledfx_set_pixel(led_idx, color);
     }
 
     if(POSITION_IN_SECOND_HALF((animation_t.iteration/4)%period, period)) {
-        leds[led_idx] = 0;
+        ledfx_set_pixel(led_idx, 0);
     }
 }
-
-static struct animation_config color_wipe_config =
-{
-    .id = 0x4e,
-    .leds_update = color_wipe,
-    .num = 1,
-};
-
 
 #endif // __COLOR_WIPE_H__
